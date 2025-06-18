f23a215922a016f095d38705b3763cf3c3b4f530
Ben <benawhiting@gmail.com>
2025-06-18 16:58:02 -0700

    Set up model for Comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :posts
+  has_many :comments
   validates :username, presence: true, uniqueness: true, length: { in: 3..24 }
   validates :password, presence: true, length: { in: 8..64 }
 end
